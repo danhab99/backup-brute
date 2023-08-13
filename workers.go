69e844ec8a96df88cbacabd71a9306e98a664772
Dan Habot <dan.habot@gmail.com>
2023-08-13 01:40:38 -0400

    added workers

---
+++ workers.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"sync"
+)
+
+func chanWorker[InutType any, OutputType any](inputChan chan InutType, workerCount int, processTask func(in InutType) OutputType) chan OutputType {
+	outputChan := make(chan OutputType)
+	var wg sync.WaitGroup
+	wg.Add(workerCount)
+
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			defer wg.Done()
+			for in := range inputChan {
+				outputChan <- processTask(in)
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(outputChan)
+	}()
+
+	return outputChan
+}
+
+func syncronizeBuffers(in chan IndexedBuffer) io.ReadCloser {
+	reader, writer := io.Pipe()
+	go func() {
+		buffMap := make(map[int]*bytes.Buffer)
+		count := 0
+
+		for buff := range in {
+			buffMap[buff.i] = buff.buffer
+
+			for buf, ok := buffMap[count]; ok; {
+				check(io.Copy(writer, buf))
+				delete(buffMap, count)
+				count++
+			}
+		}
+		check0(writer.Close())
+	}()
+	return reader
+}
+
+func makeChunks(in io.ReadCloser, chunkSize int64) (out chan IndexedBuffer) {
+	out = make(chan IndexedBuffer)
+	go func() {
+		i := 0
+		for {
+			chunk := new(bytes.Buffer)
+			n, err := io.CopyN(chunk, in, chunkSize)
+			out <- IndexedBuffer{chunk, i}
+			i++
+
+			if n > chunkSize || err != nil {
+				close(out)
+				break
+			}
+		}
+	}()
+	return
+}
